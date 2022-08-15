345ab9a4849c2cfd981a55fe21ec6e38e7178cae
annacarolinamekarupereira <annacarolinamekarupereira@gmail.com>
2022-08-15 23:02:00 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+}
+
+function  destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+
+function  destructivelyRemoveLastCat(){
+    cats.pop('Bob');
+}
+
+function   destructivelyRemoveFirstCat(){
+    cats.shift('Bob');
+}
+
+function appendCat(){
+
+    const copyOfCats = cats.slice();
+    copyOfCats.push('Broom'); 
+    return copyOfCats;
+}
+
+function prependCat(){
+
+    const copyOfCats = cats.slice();
+    copyOfCats.unshift('Arnold'); 
+    return copyOfCats;
+}
+
+function removeLastCat(){
+
+    const copyOfCats = cats.slice(0, cats.length - 1);
+    return copyOfCats;
+}
+
+function removeFirstCat(){
+
+    const copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
+
+
+
+
